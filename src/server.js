80e102abca5770faad59254f7cc04d720d638c80
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2021-05-16 22:47:01 -0400

    Register User w/ Salted Hash Password

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const Express = require('express');
 const app = Express();
 const  db = require("../db");
+const path = require('path');
+const bcrypt = require('bcryptjs');
 
+const clientAppDirectory = path.join(__dirname, '../public', 'build');
+
+app.use(Express.json());
+app.use(Express.static(clientAppDirectory));
 
 app.get("/getAll", async (req, res) => {
 
@@ -10,6 +16,24 @@ app.get("/getAll", async (req, res) => {
     res.status(200).json({
         status: "success"
     })
+});
+
+
+app.post('/api/users', async (request, response) => {
+    console.log('A request came in with the body: ' + JSON.stringify(request.body));
+    const { firstname, lastname, emailaddress, password } = request.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await db.query(
+            "INSERT INTO users (firstname, lastname, emailaddress, password) values ($1, $2, $3, $4) returning *",
+                [firstname, lastname, emailaddress, hashedPassword]
+            );
+       console.log(firstname, lastname, emailaddress, hashedPassword);
+       return response.sendStatus(200);
+    } catch(error) {
+        console.error('Something went wrong while creating a new user: ' + error.message);
+        return response.sendStatus(400);
+    }
 });
 
 const port = process.env.PORT || 4100;
