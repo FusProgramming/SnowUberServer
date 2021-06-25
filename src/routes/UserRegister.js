c13b3395d38735901ad99fc046f19dee0893e394
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2021-06-25 12:54:07 -0400

    Registration works, logs user in PostGresSQL

--- src/routes/UserRegister.js
+++
@@ -1,8 +0,0 @@
-const Express = require('express');
-const server = Express();
-const userController = require('../controller/UserController');
-console.log('1');
-
-server.post('/register', userController.register);
-
-module.exports = server;
--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ const  db = require("./db");
 const path = require('path');
 const bcrypt = require('bcryptjs');
 const jwtGenerator = require('../utils/jwtGenerator');
-const userRegisterAuth = require('./routes/UserRegister');
+const cors = require("cors");
 
 const clientAppDirectory = path.join(__dirname, '../public', 'build');
 
+app.use(cors());
 app.use(Express.json());
 app.use(Express.static(clientAppDirectory));
 
@@ -40,13 +41,36 @@ app.post("/login", async (request, response)=> {
 
     } catch(error) {
         console.error(error);
-        response.status(500).send("Server error");
+        response.status(400).send("Server error");
     }
 });
 
 //--------------------------------------------------------------------------------------------------------------------------
 
-app.use('/', userRegisterAuth);
+app.post('/register', async (request, response) => {
+    console.log('A request came in with the body: ' + JSON.stringify(request.body));
+    const {firstName, lastName, emailAddress, emailAddress2, userPassword } = request.body;
+    try {
+        const user = await db.query("SELECT * FROM users WHERE emailAddress = $1", [emailAddress]);
+
+        if(user.rows.length > 0) {
+            return response.status(401).json("User already exist!");
+        }
+
+        const hashedPassword = await bcrypt.hash(userPassword, 12);
+
+        const newUser = await db.query(
+            "INSERT INTO users (firstName, lastName, emailAddress, emailAddress2, userPassword, userPassword2) values ($1, $2, $3, $4, $5, $6) returning *",
+            [firstName, lastName, emailAddress, emailAddress2, hashedPassword, hashedPassword]
+        );
+        return response.sendStatus(200);
+    } catch(error) {
+        console.error('Something went wrong while creating a new user: ' + error.message);
+        return response.sendStatus(400);
+    }
+
+});
+
 
 const port = process.env.PORT || 4100;
 app.listen(port, () => console.log(`Server has started on localhost:${port}`));
